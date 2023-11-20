f2e1a1d6666530985c85c9d7556bec4dd8d99da7
Enrique Izel <eleaoizel@gmail.com>
2023-11-20 10:32:28 -0400

    feat: viewHolder implementation

--- app/src/main/java/com/example/recyclerviewapplication/GreenAdapter.kt
+++ app/src/main/java/com/example/recyclerviewapplication/GreenAdapter.kt
@@ -3,6 +3,7 @@ package com.example.recyclerviewapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class GreenAdapter : RecyclerView.Adapter<GreenAdapter.NumberViewHolder>() {
@@ -25,6 +26,14 @@ class GreenAdapter : RecyclerView.Adapter<GreenAdapter.NumberViewHolder>() {
   }
 
   inner class NumberViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private var numberTextView: TextView
 
+    init {
+      numberTextView = view.findViewById(R.id.text_item_number)
+    }
+
+    fun bind(textValue: String) {
+      numberTextView.text = textValue
+    }
   }
 }
